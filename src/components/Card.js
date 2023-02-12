e1bc68922f4234b78e7e9cd78e2fc09d6cd7c73f
Eren <erencan_inanci@hotmail.com>
2023-02-12 03:35:42 +0300

    rendering card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,5 @@
 import React from "react";
 
-export default function Card() {
-  return <div></div>;
+export default function Card(props) {
+  return <img className="Card" src={props.image} alt={props.name} />;
 }
--- src/components/Deck.js
+++ src/components/Deck.js
@@ -3,7 +3,7 @@ import { useState } from "react";
 import axios from "axios";
 import { API_URL } from "../helpers/API";
 import Card from "./Card";
-// import "./Deck.css";
+import "../styles/Deck.css";
 
 export default function Deck() {
   const [deck, setDeck] = useState(null);
@@ -11,37 +11,54 @@ export default function Deck() {
 
   React.useEffect(() => {
     async function getDeck() {
-      let deck = await axios.get(API_URL);
+      let deck = await axios.get(`${API_URL}/new/shuffle/`);
       setDeck(deck.data);
     }
     getDeck();
   }, [setDeck]);
 
-  //   const getCard = async () => {
-  //     let id = deck.deck_id;
-  //     try {
-  //       let cardUrl = `${API_URL}/${id}/draw/`;
-  //       let cardRes = await axios.get(cardUrl);
-  //       if (!cardRes.data.success) {
-  //         throw new Error("No card remaining!");
-  //       }
-  //       let card = cardRes.data.cards[0];
-  //       setDrawn((d) => [
-  //         ...d,
-  //         {
-  //           id: card.code,
-  //           image: card.image,
-  //           name: `${card.value} of ${card.suit}`,
-  //         },
-  //       ]);
-  //     } catch (err) {
-  //       alert(err);
-  //     }
-  //   };
+  const getCard = async () => {
+    //make request using deck id
+    //set state using new card info from api
+    let id = deck.deck_id;
+    try {
+      let cardUrl = `${API_URL}/${id}/draw/`;
+      let cardRes = await axios.get(cardUrl);
+      if (!cardRes.data.success) {
+        throw new Error("No card remaining!");
+      }
+      let card = cardRes.data.cards[0];
+      setDrawn((d) => [
+        ...d,
+        {
+          id: card.code,
+          image: card.image,
+          name: `${card.value} of ${card.suit}`,
+        },
+      ]);
+    } catch (err) {
+      alert(err);
+    }
+  };
 
   return (
     <div>
       <h1>Card Dealer</h1>
+      <button
+        onClick={() => {
+          getCard();
+        }}
+      >
+        Get Card!
+      </button>
+      <div className="Deck-cardarea">
+        {
+          // map over drawn cards and make a card for each one
+          drawn.map((c) => (
+            <Card key={c.id} name={c.name} image={c.image} />
+          ))
+        }
+      </div>
     </div>
   );
 }
